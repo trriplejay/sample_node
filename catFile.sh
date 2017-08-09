bbfa81418bcf6b93511fee64b50aff0439462bea
John Stockbauer <john@shippable.com>
2017-08-09 16:33:09 -0700

    Update catFile.sh

--- catFile.sh
+++ catFile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cat Ulysses.txt
 echo "hello world" 1>&2
 
 for i in `seq 1 10`;
 do
-  cat Ulysses.txt
+  while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "$line"
+  done < Ulysses.txt
 done
